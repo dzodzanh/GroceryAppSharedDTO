c0a1d222d921e620dff2ac4e8184c6eb59e58afa
Le Minh Thao <thaole@vulcanlabs.co>
2024-08-07 12:07:58 +0700

    Add GroceryCategoryRequestDTO & GroceryCategoryResponseDTO

--- Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryCategoryResponseDTO.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+public struct GroceryCategoryResponseDTO: Codable {
+    public var id: UUID
+    public var title: String
+    public var colorCode: String
+    
+    public init(id: UUID, title: String, colorCode: String) {
+        self.id = id
+        self.title = title
+        self.colorCode = colorCode
+    }
+}
